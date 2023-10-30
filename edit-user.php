ba7c998afa2ea2cbb69d2b48d6bbb0ff1d8862ee
Tihasha Rafa <rafatisha.98@gmail.com>
2023-10-30 23:04:33 +0600

    Edit user page design updated

--- edit-user.php
+++ edit-user.php
@@ -54,19 +54,52 @@ if ($_SERVER["REQUEST_METHOD"] === "POST" && isset($_POST["update"])) {
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Edit User</title>
+
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    <h2>Edit User: <?php echo $editUserDetails['storedUsername']; ?></h2>
+
+<div class="row">
+                <div class="col-md-6 offset-md-3">
+                    <div class="card">
+<div class="card-header bg-primary text-white">
+<h2>Edit User: <?php echo $editUserDetails['storedUsername']; ?></h2>
+                        </div>
+                        <div class="card-body">
     <form method="post" action="edit-user.php?username=<?php echo $editUserDetails['storedUsername']; ?>">
-        Email: <input type="email" name="email" value="<?php echo $editUserDetails['storedEmail']; ?>"><br>
-        Username: <input type="text" name="username" value="<?php echo $editUserDetails['storedUsername']; ?>"><br>
-        Password: <input type="password" name="password" value="<?php echo $editUserDetails['storedPassword']; ?>"><br>
+    <div class="form-group">
+                                    <label for="new_username">Email:</label>
+                                    <input type="email" name="email" class="form-control"  value="<?php echo $editUserDetails['storedEmail']; ?>">
+                                </div>
+
+
+                                <div class="form-group">
+                                    <label for="new_username">Username:</label>
+                                    <input type="text" name="username" class="form-control" value="<?php echo $editUserDetails['storedUsername']; ?>">
+                                </div>
+
+                                <div class="form-group">
+                                    <label for="new_username">Password:</label>
+                                    <input type="password" name="password"  class="form-control" value="<?php echo $editUserDetails['storedPassword']; ?>">
+                                </div>
+      
+       
         <input type="submit" name="update" value="Update">
     </form>
     <a href="index.php">Back to User List</a>
+
+    </div>
+    </div>
+    </div>
+    </div>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
 </body>
 </html>
